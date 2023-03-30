45f489a61c671e2b4165bfe1878f56926e744b24
João Porcel <68911216+AX414@users.noreply.github.com>
2023-03-29 23:38:42 -0300

    Create main.py

---
+++ pythonProject/main.py
@@ -0,0 +1,184 @@
+import discord
+import os
+import asyncio
+import ffmpeg
+import random
+import json
+import requests
+from deep_translator import GoogleTranslator
+import yt_dlp as youtube_dl
+from dotenv import load_dotenv
+from discord.ext import commands,tasks
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv('POKE')
+
+print(DISCORD_TOKEN)
+
+
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!',intents=intents)
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+        'format': 'bestaudio/best',
+        'extractaudio': True,
+        'audioformat': 'mp3',
+        'outtmpl': './downloads/%(title)s.%(ext)s',
+        'restrictfilenames': True,
+        'noplaylist': True,
+        'nocheckcertificate': True,
+        'ignoreerrors': False,
+        'logtostderr': False,
+        'quiet': True,
+        'no_warnings': True,
+        'default_search': 'auto',
+        'source_address': '0.0.0.0',
+}
+
+ffmpeg_options = {'options': '-vn'}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+titulo_video = ''
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.7):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.thumbnail = data.get('thumbnail')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False,ctx):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+
+        titulo_video = str(data['title'])
+        imagem = str(data['thumbnail'])
+        #ainda dá para adicionar mais coisas
+
+        embed = mensagem("","",imagem,"**Título:  **"+titulo_video)
+        await ctx.send(embed=embed)
+        
+        return filename
+
+# Remove o comando de ajuda padrão, eu decidi criar o meu próprio
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print("Bot pronto para o uso.")
+
+@bot.command(name='mensagem', help='Demonstra como são as mensagens do bot')
+async def mensagem(ctx):
+	await ctx.send("Essa é uma mensagem comum")
+	await ctx.send("```Essa ainda é uma mensagem comum```")
+
+@bot.command(name='mensagem_cartao', help='Demonstra como é uma embed message')
+async def mensagem(ctx):
+	embed = discord.Embed()
+	embed.title = "Titulo"
+	embed.description = "Descricao"
+	embed.add_field(name="Nome do campo", value="Texto")
+	await ctx.send(embed = embed)
+
+@bot.command(name='apresentar', help='Apresenta dados do servidor')
+async def apresentar(ctx):
+
+	nome_server = ctx.guild.name
+	criador = ctx.guild.owner
+
+	embed = discord.Embed()
+	embed.title = "Dados sobre o servidor:"
+	embed.description = "Servidor: {}\nCriado por: {}\n ".format(nome_server, criador)
+	await ctx.send(embed=embed)
+
+@bot.command(name='search', help='Apresenta os dados do pokemon pesquisado')
+async def search(ctx, pokemon):
+
+	# URL da API que vou consumir
+	# link para ver a documentação se necessário: https://pokeapi.co/
+	url = "https://pokeapi.co/api/v2/pokemon/"
+	requisicao = requests.get(url+pokemon)
+	
+	pokemon = requisicao.json()	
+
+	print(pokemon['name'])
+	
+	nome = pokemon['name']
+	imagem = pokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+	peso = pokemon['weight']/10
+	altura = pokemon['height']/10
+
+	embed = discord.Embed()
+	
+	# Capitalize() é uma função de string utilizada para deixar a 
+	# primeira letra em maiúscula
+	embed.title = "Informações de {}".format(nome.capitalize())
+	
+	embed.set_thumbnail(url=imagem)
+	embed.description = "Peso: {} Kg\nAltura: {} m".format(peso,altura)
+	embed.color = 0x2ecc71
+	await ctx.send(embed=embed)
+
+
+@bot.command(name='help', help='Apresenta os comandos do seu bot')
+async def help(ctx):
+	
+	var = "/help - Apresenta os comandos do seu bot\n"
+	var+= "/mensagem - Demonstra como são as mensagens do bot\n"
+	var+= "/apresentar - Apresenta dados do servidor\n"	
+	var+= "/search - Pesquisar pokemon\n"
+
+
+	embed = discord.Embed()
+	embed.title = "Lista de Comandos:"
+	embed.description = var
+	embed.color = 0x3498db 
+	await ctx.send(embed = embed)
+
+def mensagem(title,url1,url2,description):
+
+    default = 0
+    teal = 0x1abc9c
+    dark_teal = 0x11806a
+    green = 0x2ecc71
+    dark_green = 0x1f8b4c
+    blue = 0x3498db
+    dark_blue = 0x206694
+    purple = 0x9b59b6
+    dark_purple = 0x71368a
+    magenta = 0xe91e63
+    dark_magenta = 0xad1457
+    gold = 0xf1c40f
+    dark_gold = 0xc27c0e
+    orange = 0xe67e22
+    dark_orange = 0xa84300
+    red = 0xe74c3c
+    dark_red = 0x992d22
+    lighter_grey = 0x95a5a6
+    dark_grey = 0x607d8b
+    light_grey = 0x979c9f
+    darker_grey = 0x546e7a
+    blurple = 0x7289da
+    greyple = 0x99aab5
+
+    embed = discord.Embed()
+    embed.color = magenta
+    embed.title = title
+    embed.set_thumbnail(url=url1)
+    embed.set_image(url=url2)
+    embed.description = description
+    return embed
+
+if __name__ == "__main__" :
+    bot.run(DISCORD_TOKEN)
