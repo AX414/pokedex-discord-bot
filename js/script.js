daaf66281b145b1ae035417fa166f92ecfabc7e8
AX414 <joaovictorlisboaporcel@hotmail.com>
2023-03-30 18:54:11 -0300

    Atualizando o conteudo do site

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function typeWriter(elem) {
-    const textoArray = elem.innerHTML.split('');
-    elem.innerHTML = '';
-    textoArray.forEach((letra, i) => {
-        setTimeout(() => elem.innerHTML += letra, 75 * i);
-    })
-}
-
-typeWriter(document.querySelector('.textoAnimacao'));
