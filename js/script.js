acbb22837bc13923e2aa773b20edcc398f1bf6ec
AX414 <joaovictorlisboaporcel@hotmail.com>
2023-03-30 15:08:11 -0300

    Atualizando o estilo do site

--- js/script.js
+++ js/script.js
@@ -6,21 +6,4 @@ function typeWriter(elem) {
     })
 }
 
-function atualizarIdade(elem){
-    const dataAtual = new Date();
-    const dataNascimento = new Date("2002/03/04");
-    let idade =  dataAtual.getFullYear() - dataNascimento.getFullYear();
-    const mes = dataAtual.getMonth() - dataNascimento.getMonth();
-
-    if(mes<0||(mes===0 && dataAtual.getDate() < dataNascimento.getDate())){
-        idade--;
-    }
-    console.log(idade);
-
-    elem.forEach((elemento)=>{
-        elemento.innerHTML = idade;
-    })
-}
-
 typeWriter(document.querySelector('.textoAnimacao'));
-atualizarIdade(document.querySelectorAll('.idade'));
