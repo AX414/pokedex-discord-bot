3dde30f9e8a72ca54adae38f255de8fa20e4c5ab
João Porcel <68911216+AX414@users.noreply.github.com>
2023-04-04 10:33:15 -0300

    Atualizar o main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -231,6 +231,7 @@ async def help(ctx):
 	var+= "!mensagem - Demonstra como são as mensagens do bot\n"
 	var+= "!mensagem_cartao - Demonstra como é uma embed message\n"
 	var+= "!apresentar - Apresenta dados do servidor\n"
+ var+= "!bot_info - Apresenta informações sobre o bot\n"
 	var+= "!search 'nome do pokemon ou número' - Pesquisar pokemon\n"
 
 
